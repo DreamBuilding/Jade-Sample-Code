2cf0f78d9f6284a3bdb4b1d3d91b79bab151699d
Brett Styles <brett@dreambuilding.com.au>
2016-04-05 21:02:38 +1000

    Add NuGet packages for Angular, Bootstrap and Owin Static Files. Add initial form layed out with bootstrap. Add style overrides to match design. Update startup to deliver html from views folder.

--- SampleSite/SampleSite/Startup.cs
+++ SampleSite/SampleSite/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System.Web.Http;
 using Autofac.Integration.WebApi;
 using Microsoft.Owin;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
 using Owin;
 
 [assembly: OwinStartup(typeof(SampleSite.Startup))]
@@ -21,6 +23,16 @@ namespace SampleSite
 
             // Configure Web API with the dependency resolver.
             config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            const string rootFolder = "./views";
+            var fileSystem = new PhysicalFileSystem(rootFolder);
+            var options = new FileServerOptions
+            {
+                EnableDefaultFiles = true,
+                FileSystem = fileSystem
+            };
+
+            app.UseFileServer(options);
         }
     }
 }
